ae8bd008b60decdd3a0658e831463a0d34c84f62
Will Lorand <will@milemarker3.org>
2021-06-20 14:55:17 -0700

    feature: quick impl de React Globe GL library -- works

---
+++ src/components/ReactGlobeGL.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// React Globe GL
+import Globe from 'react-globe.gl';
+
+function ReactGlobeGL() {
+  return (
+    <div>
+      <Globe />
+    </div>
+  );
+}
+
+export default ReactGlobeGL;
